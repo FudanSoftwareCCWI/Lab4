f51e85f1c207a155a4aafbf2e62200dd253b05db
cuipiQC <eaxu1025@gmail.com>
2014-06-07 20:13:46 +0800

    Refactor the code
    Change some comments and refactor

---
+++ src/wm/test/model/DictionariesTest.java
@@ -0,0 +1,89 @@
+/**
+ * Qian Cheng
+ * 2014年6月7日 下午8:11:20
+ */
+package wm.test.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author qiancheng
+ *
+ */
+public class DictionariesTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionaries#Dictionaries(java.util.List)}.
+	 */
+	@Test
+	public void testDictionaries() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionaries#getDictionary(int)}.
+	 */
+	@Test
+	public void testGetDictionaryInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionaries#getDictionary(java.lang.String)}.
+	 */
+	@Test
+	public void testGetDictionaryString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionaries#getDictionaryNames()}.
+	 */
+	@Test
+	public void testGetDictionaryNames() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionaries#getDicNumber()}.
+	 */
+	@Test
+	public void testGetDicNumber() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionaries#getTotalSize()}.
+	 */
+	@Test
+	public void testGetTotalSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionaries#produceRecord()}.
+	 */
+	@Test
+	public void testProduceRecord() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/wm/test/model/DictionaryTest.java
@@ -0,0 +1,177 @@
+/**
+ * Qian Cheng
+ * 2014年6月7日 下午8:11:00
+ */
+package wm.test.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author qiancheng
+ *
+ */
+public class DictionaryTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#Dictionary(java.lang.String, java.util.List)}.
+	 */
+	@Test
+	public void testDictionaryStringListOfWord() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#Dictionary(java.lang.String, java.util.List, int)}.
+	 */
+	@Test
+	public void testDictionaryStringListOfWordInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#getName()}.
+	 */
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#getSize()}.
+	 */
+	@Test
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#getMatchWords(java.lang.String)}.
+	 */
+	@Test
+	public void testGetMatchWords() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#getPresentWord()}.
+	 */
+	@Test
+	public void testGetPresentWord() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#getKey(int)}.
+	 */
+	@Test
+	public void testGetKey() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#getMeaning(int)}.
+	 */
+	@Test
+	public void testGetMeaning() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#getWordRecited(int)}.
+	 */
+	@Test
+	public void testGetWordRecited() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#getWordCorrect(int)}.
+	 */
+	@Test
+	public void testGetWordCorrect() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#getWordEntry(int)}.
+	 */
+	@Test
+	public void testGetWordEntry() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#getWordIndex(java.lang.String)}.
+	 */
+	@Test
+	public void testGetWordIndex() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#setPresentWord(int)}.
+	 */
+	@Test
+	public void testSetPresentWord() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#setWordRecited(int)}.
+	 */
+	@Test
+	public void testSetWordRecited() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#setWordCorrect(int, boolean)}.
+	 */
+	@Test
+	public void testSetWordCorrect() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#calAvailableSize(int)}.
+	 */
+	@Test
+	public void testCalAvailableSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#produceRecord()}.
+	 */
+	@Test
+	public void testProduceRecord() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link wm.model.Dictionary#produceRecord(int, int)}.
+	 */
+	@Test
+	public void testProduceRecordIntInt() {
+		fail("Not yet implemented");
+	}
+
+}
